b4fbeb8f3fecfdc6d84baed95f2620b4192840f6
James Straub <jstraub@whitepages.com>
2012-04-15 00:24:47 -0700

    fixed pitch calculator

--- mus/compiler.js
+++ mus/compiler.js
@@ -1,38 +1,78 @@
+(function(){
 var endTime = function(time, expr) {
-   if(expr.tag === 'seq')
-       return arguments.callee(arguments.callee(time, expr.left), expr.right);
-   if(expr.tag == 'par'){
-       var lT = arguments.callee(time, expr.left),
-           rT = arguments.callee(time, expr.right);
-       return lT > rT ? lT : rT;
-   }
-   else
-       return time + (expr.dur || expr.duration || 0);
-};
+  var end = 0
+	switch (expr.tag){
+    case 'seq':
+      end = arguments.callee(arguments.callee(time, expr.left), expr.right)
+      break
+    case 'par':
+      var lT = arguments.callee(time, expr.left),
+          rT = arguments.callee(time, expr.right)
+      end = lT > rT ? lT : rT
+      break
+    default:
+        end = time + (expr.dur || expr.duration || 0)
+	}
+  return end
+}
 
 // maybe some helper functions
-var compileT = function(time,expr) {
-    if(expr.tag === 'seq'){
-        return [].concat(
-            arguments.callee(time, expr.left),
-            arguments.callee(endTime(time, expr.left), expr.right)
-        );
-    }
-    if(expr.tag === 'par') {
-        return [].concat(
-            arguments.callee(time, expr.left),
-            arguments.callee(time, expr.right)
-        );
-    }
-	else {
-        expr.start = time;
-        return expr;
-    }
-};
+var notes = {
+	'C': 0,
+  'Cs':1,
+	'D': 2,
+  'Ds': 3,
+	'E': 4,
+	'F': 5,
+  'Fs': 6,
+	'G': 7,
+  'Gs': 8,
+	'A': 9,
+  'As': 10,
+	'B': 11,
+}
+
+var convert_pitch = function(pitch){	
+	// the zero octive starts at 16...
+  if(pitch === undefined)
+    return 0
+    
+	var note = pitch[0].toUpperCase(),
+		oct = pitch[1]
+	return 12 + notes[note] + (12 * parseInt(oct))
+}
+
+var convert = function(time,expr) {
+	var r = []
+	switch(expr.tag) {
+	    case 'seq':
+	        r = [].concat(
+	            arguments.callee(time, expr.left),
+	            arguments.callee(endTime(time, expr.left), expr.right)
+	        )
+			break
+		case 'par':
+			r = [].concat(
+	            arguments.callee(time, expr.left),
+	            arguments.callee(time, expr.right)
+	        )
+			break
+		case "note":
+		case "rest":
+	    expr.start = time
+			expr.pitch = convert_pitch(expr.pitch)
+			r = expr
+			break
+		default:
+			throw "You have a syntax error: tag '" + expr.tag + "' not supported."
+	}
+	return r;
+
+}
 
 var compile = function (musexpr) {
-    return compileT(0, musexpr);
-};
+    return convert(0, musexpr)
+}
 
 
 var melody_mus = {
@@ -40,10 +80,10 @@ var melody_mus = {
 	left: {
 		tag:'seq',
 		left:  { tag: 'seq',
-			left: {tag: 'note', pitch:'a4', dur: 250},
+			left: {tag: 'note', pitch:'a0', dur: 250},
 			right: {tag: 'rest', duration: 300}
 		},
-		right: {tag: 'note', pitch: 'b4', dur: 250}
+		right: {tag: 'note', pitch: 'c8', dur: 250}
 	},
 	right: {
 		tag:'par',
@@ -54,3 +94,4 @@ var melody_mus = {
 
 console.log(melody_mus)
 console.log(compile(melody_mus))
+})()
