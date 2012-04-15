abfae6dd5522dd0b1b13ad956c54b892e201f7d2
James Straub <jstraub@whitepages.com>
2012-04-14 23:40:14 -0700

    added rests to my compiler

--- mus/compiler.js
+++ mus/compiler.js
@@ -1,33 +1,32 @@
 var endTime = function(time, expr) {
-   if(expr.tag === 'note')
-       return time + expr.dur;
    if(expr.tag === 'seq')
        return arguments.callee(arguments.callee(time, expr.left), expr.right);
-   else {
+   if(expr.tag == 'par'){
        var lT = arguments.callee(time, expr.left),
            rT = arguments.callee(time, expr.right);
        return lT > rT ? lT : rT;
    }
+   else
+       return time + (expr.dur || expr.duration || 0);
 };
 
 // maybe some helper functions
 var compileT = function(time,expr) {
-    if(expr.tag === 'note') {
-        expr.start = endTime(time, expr) - expr.dur;
-        return [expr];
-    }
     if(expr.tag === 'seq'){
         return [].concat(
             arguments.callee(time, expr.left),
             arguments.callee(endTime(time, expr.left), expr.right)
         );
     }
-    else {
+    if(expr.tag === 'par') {
         return [].concat(
             arguments.callee(time, expr.left),
             arguments.callee(time, expr.right)
         );
-
+    }
+	else {
+        expr.start = time;
+        return expr;
     }
 };
 
@@ -40,7 +39,10 @@ var melody_mus = {
 	tag: 'seq',
 	left: {
 		tag:'seq',
-		left:  {tag: 'note', pitch:'a4', dur: 250},
+		left:  { tag: 'seq',
+			left: {tag: 'note', pitch:'a4', dur: 250},
+			right: {tag: 'rest', duration: 300}
+		},
 		right: {tag: 'note', pitch: 'b4', dur: 250}
 	},
 	right: {
