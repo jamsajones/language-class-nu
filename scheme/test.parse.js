1a5f53d26257569ec635b584ffadea36fd8fb8bc
James Straub <jstraub@whitepages.com>
2012-05-03 21:52:37 -0700

    Added Scheme parser code and tests

---
+++ scheme/test.parse.js
@@ -0,0 +1,59 @@
+
+
+describe('parse', function(){
+  
+  var parse = undefined;
+
+  before(function(){
+    parse = PEG.buildParser(lang).parse;
+  });
+
+  it('should parse correctly', function(){
+    expect(typeof parse).to.eql('function');
+  });
+
+  it('should parse empty',function(){
+      expect(parse(" ")).to.eql(undefined);
+  });
+
+  it('should parse atom', function(){
+    expect(parse("atom")).to.eql("atom");
+  });
+
+  it('should parse (+)', function(){
+    expect(parse('(+)')).to.eql(['+']);
+  });
+
+  it('should parse (+ x 3)', function(){
+    expect(parse('(+ x 3)')).to.eql(["+", "x", "3"]);
+  });
+  
+  it('should parse (+ 1 (f x 3 y))',function(){
+    expect(parse("(+ 1 (f x 3 y))")).to.eql(["+", "1", ["f", "x", "3", "y"]]);
+  });
+  
+  it('should parse newlines correctly',function(){
+    expect(parse("(+ 1 \n(f x 3 y))")).to.eql(["+", "1", ["f", "x", "3", "y"]]);
+  });
+
+  it('should ignore empty lines', function(){
+    expect(parse("(+ 1 \n\n\n(f x 3 y))")).to.eql(["+", "1", ["f", "x", "3", "y"]]);
+  });
+  it('should parse tabs correctly', function(){
+    expect(parse("(+ 1 \t\t(f x 3 y))")).to.eql(["+", "1", ["f", "x", "3", "y"]]);
+  });
+
+  it('should parse spaces correctly', function(){
+    expect(parse("(+ 1   (f x 3 y))")).to.eql(["+", "1", ["f", "x", "3", "y"]]);
+  });
+
+  it('should parse \' to quote', function(){
+    expect(parse("'(a b c)")).to.eql(["quote", ["a", "b", "c"]]);
+  });
+
+  it('should ignore comments ;;', function(){
+    expect(parse("(+ 1 \n;;Comment\n2);;one\n;; Comment")).to.eql(["+", "1", "2"]);
+  });
+})
+        
+        
